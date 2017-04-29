59aba47345806140bf7e16906efa9761af3d3213
italo jose <italo.i@live.com>
2017-04-29 15:02:24 -0300

    testes ok

--- UnitTest/ControllersTests/PessoasControllerIntegrationTest.cs
+++ UnitTest/ControllersTests/PessoasControllerIntegrationTest.cs
@@ -25,5 +25,27 @@ namespace UnitTest
 
             Assert.Equal(data.Count, 0);
         }
+
+        [Fact]
+        public async Task DeveRetornarListaDePessoas()
+        {
+            var pessoa = new Pessoa
+            {
+                Nome = "joaquim",
+                Twitter = "@teste"
+            };
+
+            await TestDataContext.AddAsync(pessoa);
+            await TestDataContext.SaveChangesAsync();
+
+            var response = await Client.GetAsync(urlRoute);
+            response.EnsureSuccessStatusCode();
+
+            var responseString = await response.Content.ReadAsStringAsync();
+            var data = JsonConvert.DeserializeObject<List<Pessoa>>(responseString);
+
+            Assert.Equal(data.Count, 1);
+            Assert.Contains(data, x => x.Nome == pessoa.Nome);
+        }
     }
 }
